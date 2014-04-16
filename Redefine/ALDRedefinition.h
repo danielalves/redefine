fe8f43b2cbe737fb5c9d431b503d412ef65f4f78
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-04-16 16:31:57 -0300

    Updating documentation

--- Redefine/ALDRedefinition.h
+++ Redefine/ALDRedefinition.h
@@ -17,21 +17,25 @@
  *  <B> Since version 1.0.2 </B>
  *
  *  Setting a redefinition in place stops a previous redefition of the same target. Hence, it it possible to create multiple
- *  redefinitions of the same class/instance selector and use them at will.
+ *  redefinitions of the same class/instance selector and use them at will. The property usingRedefinition has become KVO
+ *  compliant, so it is possible to listen to these changes.
  *
  *  Starting and stoping to use a redefinition are atomic operations, what makes ALDRedefinition thread safe.
  */
 @interface ALDRedefinition : NSObject
 
 /**
- *  Returns if the redefinition is in place
+ *  Returns if the redefinition is in place.
+ *
+ *  Since version 1.0.2, this property is KVO compliant.
+ * 
  *  @see startUsingRedefinition
  *  @see stopUsingRedefinition
  */
 @property( nonatomic, readonly )BOOL usingRedefinition;
 
 /**
- *  Creates a redefinition for a class method and sets it in place. Thus, there is no need to call startUsingRedefinition after this method.
+ *  Creates a redefinition for a class method and calls startUsingRedefinition.
  *
  *  @param aClass            The class whose method we want to redefine
  *  @param selector          The class method we want to redefine
@@ -41,12 +45,13 @@
  *
  *  @throws NSInvalidArgumentException If any argument is null or if aClass does not respond to selector
  *
+ *  @see startUsingRedefinition
  *  @see redefineClassInstances:selector:withImplementation:
  */
 +( instancetype )redefineClass:( Class )aClass selector:( SEL )selector withImplementation:(id(^)(id object, SEL selector, ...))newImplementation;
 
 /**
- *  Creates a redefinition for a instance method of a class and sets it in place. Thus, there is no need to call startUsingRedefinition after this method.
+ *  Creates a redefinition for an instance method of a class and calls startUsingRedefinition.
  *
  *  @param aClass            The class whose instance method we want to redefine
  *  @param selector          The instance method we want to redefine
@@ -56,12 +61,16 @@
  *
  *  @throws NSInvalidArgumentException If any argument is null or if instances of aClass do not respond to selector
  *
+ *  @see startUsingRedefinition
  *  @see redefineClass:selector:withImplementation:
  */
 +( instancetype )redefineClassInstances:( Class )aClass selector:( SEL )selector withImplementation:(id(^)(id object, SEL selector, ...))newImplementation;
 
 /**
- *  Sets the redefinition represented by this object in place. That is, replaces the original implementation of the selector by the new implementation.
+ *  Sets the redefinition represented by this object in place. That is, replaces the original implementation of the selector by the 
+ *  new implementation.
+ *
+ *  Since version 1.0.2, this method is atomic and stops a previous redefinition on the same target.
  *
  *  @see stopUsingRedefinition
  *  @see usingRedefinition
@@ -70,6 +79,8 @@
 
 /**
  *  Stops the redefinition represented by this object. That is, takes the original selector implementation back.
+ *
+ *  Since version 1.0.2, this method is atomic.
  *
  *  @see startUsingRedefinition
  *  @see usingRedefinition
